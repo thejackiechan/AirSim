1dae466971a66a6af907bac5a9c3d26a933b056d
JackieChan <JackieChan@LAPTOP-0U8E3P8C>
2023-11-25 14:34:34 -0800

    Add CesiumEnv

---
+++ Unreal/Environments/CesiumEnv/Source/CesiumEnv/CesiumEnv.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CesiumEnv.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CesiumEnv, "CesiumEnv" );
---
+++ Unreal/Environments/CesiumEnv/Source/CesiumEnv/CesiumEnvGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CesiumEnvGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CESIUMENV_API ACesiumEnvGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
